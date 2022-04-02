ac4541be08976fc2219f302e9f0011cb48551336
PastIsJustPast <gfd4168@nate.com>
2022-04-02 15:37:59 +0900

    add file

---
+++ week03/n21610/21610_마법사.py
@@ -0,0 +1,66 @@
+﻿## 로직
+- 구름 이동 -> 물 증가 -> 물 복사 -> 구름 생성을 반복함
+- 구름 이동 후 구름을 갱신하고, 구름 생성후 구름을 갱신
+- 구름은 경계를 넘을 수 있지만 물복사 탐색은 경계를 넘을 수 없음
+
+
+n,m = map(int,input().split())
+#graph = [list(map(int,input().split())) for _ in range(n)]
+graph = []
+for info in input().split("\n"):
+    graph.append(list(map(int,info.split())))
+magic = [list(map(int,input().split())) for _ in range(m)]
+
+
+
+## 구름이동 함수
+#input : 명령
+#output : 구름 위치
+#구름은 경계를 넘나들수 있음
+direc = {1:[0,-1],2:[-1,-1],3:[-1,0],4:[-1,1],5:[0,1],6:[1,1],7:[1,0],8:[1,-1]} #좌 좌상 상 우상 우 우하 하 좌하
+def move_cloud(d,s):
+    temp = []
+    #구름 이동
+    for x,y in cloud :
+        nx = (x + s * direc[d][0] + n ) % n #구름은 경계를 넘나들 수 있음
+        ny = (y + s * direc[d][1] + n ) % n
+        temp.append((nx,ny))
+    return temp
+
+
+##물 증가 및 복사함수/ 구름 재생성
+#input : 구름 우치
+#output : cloud
+def water(cloud):
+    #물증가
+    for x,y in cloud :
+        graph[x][y] +=1
+
+    dx = [1,1,-1,-1]
+    dy = [1,-1,-1,1]
+    #물 복사
+    for x,y in cloud :
+        #대각방향 탐색
+        for i in range(4):
+            nx,ny = x + dx[i], y + dy[i]
+            if nx <0 or nx >= n or ny <0 or ny >= n : continue #물복사 탐색은 경계를 넘지 않음
+            if graph[nx][ny] >= 1 : graph[x][y] += 1 #물 복사됨
+
+    #구름 생성
+    temp = []
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] >=2 and (x,y) not in cloud:
+                temp.append((x,y))
+                graph[x][y] -= 2
+    return temp
+
+
+#초기 구름 위치
+cloud = [(n-2,0),(n-1,1),(n-1,0),(n-2,1)]
+
+for d,s in magic :
+    cloud = move_cloud(d,s)
+    cloud = water(cloud)
+
+print(sum(map(sum,graph)))
---
+++ week04/n17837/17780_새로운게임2.py
@@ -0,0 +1,83 @@
+﻿from collections import deque
+n,k = map(int,input().split())
+#graph = [list(map(int,input().split())) for _ in range(n)]
+#chess = [list(map(int,input().split())) for _ in range(k)]
+graph = []
+chess = []
+data = input().split("\n")
+for i in range(n):
+    graph.append(list(map(int,data[i].split())))
+for j in range(n,n+ k):
+    x,y,d = map(int,data[j].split())
+    x,y,d = x-1,y-1,d-1
+    chess.append([x,y,d])
+
+#방향
+dx = [0,0,-1,1] # 우 좌 상 하
+dy = [1,-1,0,0]
+
+#체스 말의 관계, 2차원리스트 위에 순서대로 쌓임(앞에 있는 번호가 아래에 있음
+r = [[[] for _ in range(n)] for _ in range(n)]
+i = 0
+for x,y,d in chess :
+    r[x][y] += [i]
+    i +=1
+
+
+def move():
+    for i in range(k):
+        x,y,d = chess[i] # 말 위치 및 방향
+        #이동할 칸 탐색
+        nx,ny = x + dx[d] , y+ dy[d]
+
+        #이동하게 될 말 탐색
+        temp = r[x][y][r[x][y].index(i):] #i번째 체스말 포함 그 위에 말들을 모두 temp에 담음
+        del r[x][y][r[x][y].index(i):] #원래 칸에서 제거
+
+        #이동(관계 갱신) #파란색+격자밖 -> 흰색 -> 빨간색 순으로 탐색
+        if nx < 0 or nx >= n or ny <0 or ny >= n or graph[nx][ny] == 2 :  #격자 밖이나 파란칸
+            #방향 바꿔서 재 탐색
+            nd = (5 - d)% 4  # 0<->1, 2<->3
+            nx,ny = x + dx[nd] , y + dy[nd]
+            if nx < 0 or nx >= n or ny <0 or ny >= n or graph[nx][ny] == 2 : #또다시 격자 밖이나 파란칸이라면
+                r[x][y] += temp  # 그자리에 관계 다시 두기
+                for j in temp:
+                    chess[j][2] = (5- chess[j][2]) % 4   #방향 모두 바꾸기
+            else :  #격자나 파란칸이 아니라면
+                r[nx][ny] += temp
+                for j in temp :
+                    chess[j][0:2] = [nx,ny] #방향 위치 다 바꾸기
+                    chess[j][2] = (5 - chess[j][2]) % 4
+
+        elif graph[nx][ny] == 0 : #흰색으로 이동한다면/관계는 안바뀜
+            r[nx][ny] += temp #그대로 얹음
+
+            #이동(위치 갱신)
+            for j in temp:
+                chess[j][0:2] = [nx,ny] # 위치 갱신
+
+        elif graph[nx][ny] == 1 : #빨간색으로 이동한다면/관계는 다 바뀜
+            temp = temp[::-1] #관계 바꿈
+            r[nx][ny] += temp #그대로 얹음
+
+            # 이동(위치 갱신)
+            for j in temp:
+                chess[j][0:2] = [nx, ny]  # 위치 갱신
+
+
+def solution():
+    count = 0
+    while count < 999 :
+        count +=1
+        move()
+        for x in range(n):
+            for y in range(n):
+               if len(r[x][y]) == k :
+                    print(count)
+                    return
+    print(-1)
+#max([max(list(map(len, r[i]))) for i in range(k)]) == k:
+
+#solution()
+
+
